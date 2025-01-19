10a82407f411de33f13d72deb2ed86bdd1ea73da
radinals <radinalsaragih@gmail.com>
2025-01-19 23:11:45 +0700

    added new plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,8 @@
 local getConfig = function(conf) return require("plugins/config/" .. conf) end
+
 return {
 
-   {
+  {
     "goolord/alpha-nvim",
     -- dependencies = { 'echasnovski/mini.icons' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -11,9 +12,257 @@ return {
       -- if provider not loaded and enabled is true, it will try to use another provider
       startify.file_icons.provider = "devicons"
       require("alpha").setup(
-        startify.config
+      startify.config
       )
     end,
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+
+
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "astro", "glimmer", "handlebars", "html", "javascript",
+    "jsx", "markdown", "php", "rescript", "svelte", "tsx",
+    "twig", "typescript", "vue", "xml" },
+    config = function ()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          -- Defaults
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        },
+      })
+    end
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+      }
+      dap.configurations.cpp = {
+        {
+          name = "Launch C/C++ Program",
+          type = "gdb",         -- Matches the adapter name
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = true,
+          stopAtBeginningOfMainSubprogram = false,
+          args = {},
+
+          setupCommands = {
+            {
+              text = '-enable-pretty-printing',
+              description = 'Enable pretty printing',
+              ignoreFailures = false,
+            },
+          }
+
+        },
+      }
+    end,
+  },
+
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function ()
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
+    config =function ()
+      vim.fn.sign_define('DapBreakpoint', {text='●', texthl='DapBreakpoint', linehl='', numhl=''})
+      vim.fn.sign_define('DapBreakpointCondition', {text='◆', texthl='DapBreakpointCondition', linehl='', numhl=''})
+      vim.fn.sign_define('DapLogPoint', {text='✎', texthl='DapLogPoint', linehl='', numhl=''})
+      vim.fn.sign_define('DapStopped', {text='→', texthl='DapStopped', linehl='DapStoppedLine', numhl=''})
+      local dap = require("dap")
+      
+      require("dapui").setup()
+    end
+  },
+
+  {
+    'stevearc/overseer.nvim',
+    opts = {},
+    config = function()
+      require("overseer").setup()
+    end,
+  },
+
+  {
+    'Civitasv/cmake-tools.nvim',
+    dependencies = { 'stevearc/overseer.nvim' },
+    ft = {"cpp"},
+    config = function ()
+      local osys = require("cmake-tools.osys")
+      require("cmake-tools").setup {
+        cmake_command = "cmake", -- this is used to specify cmake command path
+        ctest_command = "ctest", -- this is used to specify ctest command path
+        cmake_use_preset = true,
+        cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
+        cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
+        cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
+        -- support macro expansion:
+        --       ${kit}
+        --       ${kitGenerator}
+        --       ${variant:xx}
+        cmake_build_directory = function()
+          if osys.iswin32 then
+            return "out\\${variant:buildType}"
+          end
+          return "out/${variant:buildType}"
+        end, -- this is used to specify generate directory for cmake, allows macro expansion, can be a string or a function returning the string, relative to cwd.
+        cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
+        cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
+        cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
+        cmake_variants_message = {
+          short = { show = true }, -- whether to show short message
+          long = { show = true, max_length = 40 }, -- whether to show long message
+        },
+        cmake_dap_configuration = { -- debug settings for cmake
+          name = "cpp",
+          type = "gdb",
+          request = "launch",
+          stopOnEntry = false,
+          runInTerminal = true,
+          console = "integratedTerminal",
+        },
+        cmake_executor = { -- executor to use
+          name = "quickfix", -- name of the executor
+          opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
+          default_opts = { -- a list of default and possible values for executors
+            quickfix = {
+              show = "always", -- "always", "only_on_error"
+              position = "belowright", -- "vertical", "horizontal", "leftabove", "aboveleft", "rightbelow", "belowright", "topleft", "botright", use `:h vertical` for example to see help on them
+              size = 10,
+              encoding = "utf-8", -- if encoding is not "utf-8", it will be converted to "utf-8" using `vim.fn.iconv`
+              auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
+            },
+            toggleterm = {
+              direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
+              close_on_exit = false, -- whether close the terminal when exit
+              auto_scroll = true, -- whether auto scroll to the bottom
+              singleton = true, -- single instance, autocloses the opened one, if present
+            },
+            overseer = {
+              new_task_opts = {
+                strategy = {
+                  "toggleterm",
+                  direction = "horizontal",
+                  autos_croll = true,
+                  quit_on_exit = "success"
+                }
+              }, -- options to pass into the `overseer.new_task` command
+              on_new_task = function(task)
+                require("overseer").open(
+                { enter = false, direction = "right" }
+                )
+              end,   -- a function that gets overseer.Task when it is created, before calling `task:start`
+            },
+            terminal = {
+              name = "Main Terminal",
+              prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
+              split_direction = "horizontal", -- "horizontal", "vertical"
+              split_size = 11,
+
+              -- Window handling
+              single_terminal_per_instance = true, -- Single viewport, multiple windows
+              single_terminal_per_tab = true, -- Single viewport per tab
+              keep_terminal_static_location = true, -- Static location of the viewport if avialable
+              auto_resize = true, -- Resize the terminal if it already exists
+
+              -- Running Tasks
+              start_insert = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
+              focus = false, -- Focus on terminal when cmake task is launched.
+              do_not_add_newline = false, -- Do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter.
+            }, -- terminal executor uses the values in cmake_terminal
+          },
+        },
+        cmake_runner = { -- runner to use
+          name = "terminal", -- name of the runner
+          opts = {}, -- the options the runner will get, possible values depend on the runner type. See `default_opts` for possible values.
+          default_opts = { -- a list of default and possible values for runners
+            quickfix = {
+              show = "always", -- "always", "only_on_error"
+              position = "belowright", -- "bottom", "top"
+              size = 10,
+              encoding = "utf-8",
+              auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
+            },
+            toggleterm = {
+              direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
+              close_on_exit = false, -- whether close the terminal when exit
+              auto_scroll = true, -- whether auto scroll to the bottom
+              singleton = true, -- single instance, autocloses the opened one, if present
+            },
+            overseer = {
+              new_task_opts = {
+                strategy = {
+                  "toggleterm",
+                  direction = "horizontal",
+                  autos_croll = true,
+                  quit_on_exit = "success"
+                }
+              }, -- options to pass into the `overseer.new_task` command
+              on_new_task = function(task)
+              end,   -- a function that gets overseer.Task when it is created, before calling `task:start`
+            },
+            terminal = {
+              name = "Main Terminal",
+              prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
+              split_direction = "horizontal", -- "horizontal", "vertical"
+              split_size = 11,
+
+              -- Window handling
+              single_terminal_per_instance = true, -- Single viewport, multiple windows
+              single_terminal_per_tab = true, -- Single viewport per tab
+              keep_terminal_static_location = true, -- Static location of the viewport if avialable
+              auto_resize = true, -- Resize the terminal if it already exists
+
+              -- Running Tasks
+              start_insert = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
+              focus = false, -- Focus on terminal when cmake task is launched.
+              do_not_add_newline = false, -- Do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter.
+            },
+          },
+        },
+        cmake_notifications = {
+          runner = { enabled = true },
+          executor = { enabled = true },
+          spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }, -- icons used for progress display
+          refresh_rate_ms = 100, -- how often to iterate icons
+        },
+        cmake_virtual_text_support = true, -- Show the target related to current file using virtual text (at right corner)
+      }
+
+    end
   },
 
   {
@@ -83,14 +332,14 @@ return {
 
 
   {
-      "rachartier/tiny-code-action.nvim",
+    "rachartier/tiny-code-action.nvim",
     dependencies = {
-        {"nvim-lua/plenary.nvim"},
-        {"nvim-telescope/telescope.nvim"},
+      {"nvim-lua/plenary.nvim"},
+      {"nvim-telescope/telescope.nvim"},
     },
     event = "LspAttach",
     config = function()
-        require('tiny-code-action').setup()
+      require('tiny-code-action').setup()
     end
   },
 
@@ -164,8 +413,12 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      "desdic/telescope-rooter.nvim"
+    },
     config = function()
+
       require("telescope").setup(getConfig("conf_telescope"))
     end
   },
